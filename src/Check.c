e064c8a572f298b4a6d9db50287b544cc0cad06b
hazzi125 <denchik.kiselevchik@gmail.com>
2018-05-29 00:25:09 +0700

    Second reworking the Check function

--- src/Check.c
+++ src/Check.c
@@ -15,7 +15,7 @@ int Check(int A[9][9], int n) {
 		    if (A[k][i] == A[j][i]) {
 		    	res = 0;
 			break;
-		    }		
+		    }
 		}
 	    }
 	    if (res == 0) {
@@ -23,5 +23,25 @@ int Check(int A[9][9], int n) {
 	    }
 	}
     }
+
+    int B[9], l1 = 0, l2 = 0, k;
+    for (l1 = 0; l1 < n; l1 += 3) {
+        for (l2 = 0; l2 < n; l2 += 3) {
+	    k = 0;
+	    for (int i = l1; i < l1 + 3; i++) {
+		for (int j = l2; j < l2 + 3; j++) {
+		    B[k] = A[i][j];
+		    k++;
+		}
+	    }
+	    for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+		    if ((i != j) && (B[i] == B[j])) {
+			res = 0;
+		    }		
+		}
+	    }
+	}
+    } 
     return res;
 }
