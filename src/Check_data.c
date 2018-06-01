3a9b305556ce40a4c7901adc3500e4db17760c83
hazzi125 <denchik.kiselevchik@gmail.com>
2018-06-02 00:39:02 +0700

    Adding menu

--- src/Check_data.c
+++ src/Check_data.c
@@ -5,9 +5,9 @@
 #include "func.h"
 
 int Check_data(int x, int y, int num) {
-    int res = 1;
-    if ((x < 1) || (x > 9) || (y < 1) || (y > 9) || (num < 0) || (num > 9)) {
-        res = 0;
+    int res = 0;
+    if ((x >= 1) && (x <= 9) && (y >= 1) && (y <= 9) && (num >= 0) && (num <= 9)) {
+        res = 1;
     }
     return res;
 }
--- src/main.c
+++ src/main.c
@@ -5,84 +5,123 @@
 #include "func.h"
 
 int main() {
-    srand(time(NULL));
-    int n = 9, x, y, num;
-    int A[9][9];
-    bool flag = 0;
-    FILE *f1 = fopen("/home/denis/trpo/Kursach/src/file1.txt", "r");
-    system("clear");
-    printf("    1 2 3   4 5 6   7 8 9  \n");
-    printf("  |------------------------\n");
-    for (int i = 0; i < n; i++) {
-        printf(" %d|", i + 1);
-        for (int j = 0; j < n; j++) {
-            fscanf(f1, "%1d", &A[i][j]);
-            if (A[i][j] != 0) {
-                if ((j + 1) % 3 == 0) {
-                    printf(" %d |", A[i][j]);
+
+    char c;
+    printf("1. New game\n");
+    printf("2. Rules\n");
+    printf("3. Exit\n");
+
+    while (c != '3') {
+        c = getchar();
+        switch(c) {
+            case '1': {
+                srand(time(NULL));
+		int n = 9, x, y, num;
+		int A[9][9];
+		bool flag = 0;
+		FILE *f1 = fopen("/home/denis/trpo/Kursach/src/file1.txt", "r");
+		system("clear");
+		printf("    1 2 3   4 5 6   7 8 9  \n");
+		printf("  |------------------------\n");
+		for (int i = 0; i < n; i++) {
+		    printf(" %d|", i + 1);
+		    for (int j = 0; j < n; j++) {
+			fscanf(f1, "%1d", &A[i][j]);
+			if (A[i][j] != 0) {
+		            if ((j + 1) % 3 == 0) {
+			        printf(" %d |", A[i][j]);
+			    }
+			    else {
+			        printf(" %d", A[i][j]);
+			    }
+			}
+			else {
+		            if ((j + 1) % 3 == 0) {
+			        printf("   |");
+			    }
+			    else {
+				printf("  ");
+			    }
+			}
+		    }
+		    printf("\n");
+		    if ((i + 1) % 3 == 0) {
+		        printf("  -------------------------\n");	
+		    }
+		}
+		fclose(f1);
+		while (flag == 0) {
+	            printf("Enter column number:\n");
+		    scanf("%d", &x);
+		    printf("Enter string number:\n");
+		    scanf("%d", &y);
+		    x--; y--;
+		    printf("Enter the number you want to put in the cell:\n");
+		    scanf("%d", &num);
+		    system("clear");
+		    if (Check_data(x + 1, y + 1, num) == 1) {
+	                A[x][y] = num;
+			flag = Check(A, n);
+		    }
+		    else {
+		        printf("You entered wrong parameters.\nTry again.\n");
+		    }
+		    printf("    1 2 3   4 5 6   7 8 9  \n");
+		    printf("  |------------------------\n");
+		    for (int i = 0; i < n; i++) {
+			printf(" %d|", i + 1);
+			for (int j = 0; j < n; j++) {
+		            if (A[i][j] != 0) {
+				if ((j + 1) % 3 == 0) {
+				    printf(" %d |", A[i][j]);
+				}
+				else {
+				    printf(" %d", A[i][j]);
+				}
+		            }
+			    else {
+				if ((j + 1) % 3 == 0) {
+				    printf("   |");
+				}
+				else {
+				   printf("  ");
+				}
+				}
+			    }
+			    printf("\n");
+			    if ((i + 1) % 3 == 0) {
+				printf("  -------------------------\n");	
+			    }
+		            }
+		        }
+		        printf("Congratulations!!! You won!!!\n");
                 }
-                else {
-                    printf(" %d", A[i][j]);
-                }
+            
+                case '2': {
+                    system("clear");
+                    FILE *f = fopen("/home/denis/trpo/Kursach/src/file2.txt", "r");
+                    char ch;
+                    int k = 0;
+                    printf(" ");
+                    while (!feof(f)) {
+                        ch = getc(f);
+                        if (ch == ' ') {
+                            k++;
+                        }
+                        if (k == 10) {
+                            printf("\n");
+	                    k = 0;
+	                }
+			printf("%c", ch);
+		    }
+		    printf("\n");
+	            fclose(f);
+                    break;
+                
             }
-            else {
-                if ((j + 1) % 3 == 0) {
-                    printf("   |");
-                }
-                else {
-                    printf("  ");
-                }
+            case '3': {
+                return 0;
             }
         }
-        printf("\n");
-        if ((i + 1) % 3 == 0) {
-            printf("  -------------------------\n");	
-        }
-    }
-    fclose(f1);
-    while (flag == 0) {
-        printf("Enter column number:\n");
-        scanf("%d", &x);
-        printf("Enter string number:\n");
-        scanf("%d", &y);
-        x--; y--;
-        printf("Enter the number you want to put in the cell:\n");
-        scanf("%d", &num);
-        system("clear");
-        if (Check_data(x + 1, y + 1, num) == 1) {
-            A[x][y] = num;
-            flag = Check(A, n);
-        }
-        else {
-            printf("You entered wrong parameters.\nTry again.\n");
-        }
-        printf("    1 2 3   4 5 6   7 8 9  \n");
-        printf("  |------------------------\n");
-        for (int i = 0; i < n; i++) {
-            printf(" %d|", i + 1);
-            for (int j = 0; j < n; j++) {
-                if (A[i][j] != 0) {
-                    if ((j + 1) % 3 == 0) {
-                        printf(" %d |", A[i][j]);
-                    }
-                    else {
-                        printf(" %d", A[i][j]);
-                    }
-                }
-                else {
-                    if ((j + 1) % 3 == 0) {
-                        printf("   |");
-                    }
-                    else {
-                        printf("  ");
-                    }
-                }
-            }
-            printf("\n");
-            if ((i + 1) % 3 == 0) {
-                printf("  -------------------------\n");	
-            }
-        }
-    }
-    printf("Congratulations!!! You won!!!\n");
+    }             
 }
